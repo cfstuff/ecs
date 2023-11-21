4fd61ae2e225bc62c3d5f5bac8bada8d6b505d7c
cfstuff <137679383+cfstuff@users.noreply.github.com>
2023-11-21 14:54:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ async function handleRequest(request) {
     let res = r404;
     const { method, headers, url } = request
     let clientIp =headers.get('CF-Connecting-IP');
-    clientIp = '182.239.127.137';//hkm data center
+    clientIp = '182.239.127.137';//hk ip.test on https://dns.google/query
     const sourcePrefixLength = clientIp.includes(':') ? 48 : 24;
-    //const ecs = '&edns_client_subnet=221.179.3.0/24&ecs=221.179.3.0/24';//clientIp?`&edns_client_subnet=${clientIp}/${sourcePrefixLength}`:'';
     const searchParams = new URL(url).searchParams
 
     if (method == 'GET' && searchParams.has('dns')) {
